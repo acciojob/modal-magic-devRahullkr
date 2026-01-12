89a6157ac9ff5ebf38550afc1ada3bbc28fc67d4
Rahul Kr <rahulkumar8604745@gmail.com>
2026-01-12 23:20:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const openModalBtn = document.getElementById("openModal");
 const modalOverlay = document.querySelector(".modal-overlay");
+const modal = document.querySelector(".modal");
 const closeModalBtn = document.querySelector(".close-modal");
 
 // Open modal
 openModalBtn.addEventListener("click", () => {
   modalOverlay.style.display = "flex";
+  modal.style.display = "block";
 });
 
 // Close modal via close button
 closeModalBtn.addEventListener("click", () => {
   modalOverlay.style.display = "none";
+  modal.style.display = "none";
 });
 
-// Close modal by clicking outside
-modalOverlay.addEventListener("click", (event) => {
-  if (event.target === modalOverlay) {
+// Close modal on outside click
+modalOverlay.addEventListener("click", (e) => {
+  if (e.target === modalOverlay) {
     modalOverlay.style.display = "none";
+    modal.style.display = "none";
   }
 });
